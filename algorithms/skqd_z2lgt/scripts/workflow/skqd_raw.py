562b9bd9c8464d17f35281cfe3f9cb847ff7f925
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-10-14 16:53:06 +0900

    pmap

--- algorithms/skqd_z2lgt/scripts/workflow/skqd_raw.py
+++ algorithms/skqd_z2lgt/scripts/workflow/skqd_raw.py
@@ -10,11 +10,11 @@ from skqd_z2lgt.sqd import sqd
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
-    parser.add_argument('--gpu')
+    parser.add_argument('--gpu', nargs='+')
     options = parser.parse_args()
 
     if options.gpu:
-        os.environ['CUDA_VISIBLE_DEVICES'] = options.gpu
+        os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(options.gpu)
     os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
     jax.config.update('jax_enable_x64', True)
 
@@ -33,7 +33,11 @@ if __name__ == '__main__':
     ising_hamiltonian = dual_lattice.make_hamiltonian(configuration['plaquette_energy'])
 
     states = exp_plaq_data.reshape(-1, num_plaq)[:, ::-1]
-    sqd_states, ham_proj, energy, _ = sqd(ising_hamiltonian, states)
+    if not options.gpu or len(options.gpu) == 1:
+        device_id = 0
+    else:
+        device_id = -1
+    sqd_states, ham_proj, energy, eigvec = sqd(ising_hamiltonian, states, jax_device_id=device_id)
 
     with h5py.File(options.filename, 'r+') as out:
         try:
@@ -45,6 +49,7 @@ if __name__ == '__main__':
         group.create_dataset('num_plaq', data=num_plaq)
         group.create_dataset('sqd_states', data=np.packbits(sqd_states, axis=1))
         group.create_dataset('energy', data=energy)
+        group.create_dataset('eigvec', data=eigvec)
         subgroup = group.create_group('ham_proj')
         subgroup.create_dataset('data', data=ham_proj.data)
         subgroup.create_dataset('indices', data=ham_proj.indices)
