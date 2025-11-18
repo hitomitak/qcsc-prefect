9709d4048f1af9df7fcd15476f1adbf33cd7e2b7
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-11-18 10:13:35 +0900

    detailed validation

--- algorithms/skqd_z2lgt/skqd_z2lgt/tasks/open_output.py
+++ algorithms/skqd_z2lgt/skqd_z2lgt/tasks/open_output.py
@@ -3,7 +3,20 @@ import os
 import logging
 from typing import Optional
 from pathlib import Path
+from pydantic import BaseModel
 from skqd_z2lgt.parameters import Parameters
+
+
+def compare_models(model1: BaseModel, model2: BaseModel, _current: str = ''):
+    for field in type(model1).model_fields:
+        value1 = getattr(model1, field)
+        value2 = getattr(model2, field)
+        print(f'Comparing {_current}{field}: {isinstance(value1, BaseModel)} {value1} {value2}')
+        if isinstance(value1, BaseModel):
+            if (mismatch := compare_models(value1, value2, f'{_current}{field}.')):
+                return mismatch
+        elif value1 != value2:
+            return f'{_current}{field}', value1, value2
 
 
 def open_output(parameters: Parameters, logger: Optional[logging.Logger] = None) -> str:
@@ -23,8 +36,9 @@ def open_output(parameters: Parameters, logger: Optional[logging.Logger] = None)
         with open(path, 'r', encoding='utf-8') as source:
             params = Parameters.model_validate_json(source.read())
 
-        if params != parameters:
-            raise RuntimeError('Saved parameters do not match')  # Should show where
+        if (mismatch := compare_models(params, parameters)):
+            raise RuntimeError(f'Saved parameters do not match at {mismatch[0]}:'
+                               f' {mismatch[1]} != {mismatch[2]}')
 
     else:
         logger.info('Creating a new file %s', parameters.pkgpath)
