191324e8a881d60aeec91f638e974de83f02df72
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-12-30 06:53:38 +0900

    bugfix

--- algorithms/skqd_z2lgt/skqd_z2lgt/tasks/preprocess.py
+++ algorithms/skqd_z2lgt/skqd_z2lgt/tasks/preprocess.py
@@ -109,11 +109,14 @@ def preprocess(
     """Standalone preprocess function."""
     def convert_fn(tasks):
         dual_lattice = make_dual_lattice(parameters)
-        batch_size = parameters.runtime.shots // (os.cpu_count() - 1)
+        batch_size = {
+            'exp': parameters.runtime.shots_exp // (os.cpu_count() - 1),
+            'ref': parameters.runtime.shots_ref // (os.cpu_count() - 1)
+        }
 
         for etype, idt, ikrylov in tasks:
             bit_array = load_raw(parameters, etype, idt, ikrylov)
-            arrays = convert_link_to_plaq(bit_array, dual_lattice, batch_size=batch_size)
+            arrays = convert_link_to_plaq(bit_array, dual_lattice, batch_size=batch_size[etype])
             save_reco(parameters, arrays, etype, idt, ikrylov, logger=logger)
 
     preprocess_flow(parameters, convert_fn, logger=logger)
